b4f66d5312be3831033a213751dcc933422fa740
James Turk <james.p.turk@gmail.com>
2018-11-28 14:47:09 -0500

    add pyproject.toml

--- clean.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-black -l 99 --py36 saturn
--- setup.py
+++ setup.py
@@ -4,35 +4,29 @@ from saturn import __version__
 
 long_description = open("README.md").read()
 
-setup(name='saturn',
-      version=__version__,
-      packages=find_packages(),
-      author='James Turk',
-      author_email='jpturk@pbs.org',
-      license='MIT',
-      url='https://github.com/pbs/saturn/',
-      description='tool for working with ECS tasks',
-      long_description=long_description,
-      platforms=['any'],
-      zip_safe=False,
-      entry_points='''[console_scripts]
-  saturn = saturn.cli:cli''',
-      install_requires=[
-          'Click==7.0',
-          'tabulate==0.8.2',
-          'boto3',
-      ],
-      extras_require={
-          'dev': [
-            'black',
-          ],
-      },
-      classifiers=["Development Status :: 3 - Alpha",
-                   "Intended Audience :: Developers",
-                   "License :: OSI Approved :: MIT License",
-                   "Natural Language :: English",
-                   "Operating System :: OS Independent",
-                   "Programming Language :: Python :: 3.6",
-                   "Programming Language :: Python :: 3.7",
-                   ],
-      )
+setup(
+    name="saturn",
+    version=__version__,
+    packages=find_packages(),
+    author="James Turk",
+    author_email="jpturk@pbs.org",
+    license="MIT",
+    url="https://github.com/pbs/saturn/",
+    description="tool for working with ECS tasks",
+    long_description=long_description,
+    platforms=["any"],
+    zip_safe=False,
+    entry_points="""[console_scripts]
+  saturn = saturn.cli:cli""",
+    install_requires=["Click==7.0", "tabulate==0.8.2", "boto3"],
+    extras_require={"dev": ["black"]},
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+)
